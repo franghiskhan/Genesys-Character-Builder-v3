32b040cef1314c7eb9c61818df0ce810c350e14e
franghiskhan <franghiskhan@gmail.com>
2019-02-17 00:10:14 -0500

    Changed tabBody for Skills and Talents to panels
    Because tab controls cannot be made transparent, I set Visible to false for each a Skil and Talent panel, and set a button to set the correct panel to visible.

--- Genesys Character Builder/Genesys Character Builder/frmGenesys.cs
+++ Genesys Character Builder/Genesys Character Builder/frmGenesys.cs
@@ -58,8 +58,8 @@ namespace Genesys_Character_Builder
             Features = "",
         };
 
-    private SkillsTemplate[] terrinothSkills = new SkillsTemplate[]
-    {
+        private SkillsTemplate[] terrinothSkills = new SkillsTemplate[]
+        {
             //general
             new SkillsTemplate("Alchemy", "Int", "Description", false, 0), //0
             new SkillsTemplate("Athletics", "Br", "Description", false, 0), //1
@@ -110,10 +110,10 @@ namespace Genesys_Character_Builder
             new SkillsTemplate("Custom Skill", "Int", "Description", false, 0), //41
             new SkillsTemplate("Custom Skill", "Int", "Description", false, 0), //42
             new SkillsTemplate("Custom Skill", "Int", "Description", false, 0), //43
-    };
+        };
 
-    private SkillsTemplate[] androidSkills = new SkillsTemplate[]
-    {
+        private SkillsTemplate[] androidSkills = new SkillsTemplate[]
+        {
             //general
             new SkillsTemplate("Athletics", "Br", "Description", false, 0), //0
             new SkillsTemplate("Comp (Hacking)", "Int", "Description", false, 0), //1
@@ -164,10 +164,10 @@ namespace Genesys_Character_Builder
             new SkillsTemplate("Custom Skill", "Int", "Description", false, 0), //41
             new SkillsTemplate("Custom Skill", "Int", "Description", false, 0), //42
             new SkillsTemplate("Custom Skill", "Int", "Description", false, 0), //43
-    };
+        };
 
-    private TalentsTemplate[] talentsList =
-    {
+        private TalentsTemplate[] talentsList =
+        {
             //tier 1
             new TalentsTemplate("Talent", "Description", false, false, 1, 0), //0
             new TalentsTemplate("Talent", "Description", false, false, 1, 0), //1
@@ -199,8 +199,9 @@ namespace Genesys_Character_Builder
             new TalentsTemplate("Talent", "Description", false, false, 5, 0), //23
             new TalentsTemplate("Talent", "Description", false, false, 5, 0), //24
         };
+        private object transparentTabControl1;
 
-    private void frmGenesys_Load()
+        private void frmGenesys_Load(object sender, EventArgs e)
         {
             cboSetting.Items.Clear();
             cboSetting.Items.Add("Android");
@@ -227,9 +228,6 @@ namespace Genesys_Character_Builder
             lblStrainCurrent.Text = myCharacter.StrainCurrent.ToString();
             lblDefenseMelee.Text = myCharacter.DefenseMelee.ToString();
             lblDefenseRanged.Text = myCharacter.DefenseRanged.ToString();
-
-            Array.Copy(terrinothSkills, myCharacter.Skills, myCharacter.Skills.Length);
-            Array.Copy(talentsList, myCharacter.Talents, myCharacter.Talents.Length);
         }
 
         private void cboSetting_SelectedIndexChanged(object sender, EventArgs e)
@@ -247,6 +245,7 @@ namespace Genesys_Character_Builder
                     
                     cboSpecies.SelectedIndex = 0;
                     cboCareer.Items.Clear();
+                    lblSkillsMagic.Text = "Custom Skills";
                     Array.Copy(terrinothSkills, myCharacter.Skills, myCharacter.Skills.Length);
                     Array.Copy(talentsList, myCharacter.Talents, myCharacter.Talents.Length);
                     break;
@@ -276,65 +275,161 @@ namespace Genesys_Character_Builder
                     cboCareer.Items.Add("Scout");
                     cboCareer.Items.Add("Warrior");
                     cboCareer.SelectedIndex = 0;
+                    lblSkillsMagic.Text = "Magic Skills";
                     Array.Copy(terrinothSkills, myCharacter.Skills, myCharacter.Skills.Length);
                     Array.Copy(talentsList, myCharacter.Talents, myCharacter.Talents.Length);
                     break;
             }
-
+            updateLabelColors();
             updateSkills();
         }
 
-        private void updateSkills()
+        private void updateLabelColors()
+        {
+            switch (myCharacter.Setting)
+            {
+            case "Android":
+                lblBrawnLabel.ForeColor = Color.Blue;
+                lblAgilityLabel.ForeColor = Color.Blue;
+                lblIntellectLabel.ForeColor = Color.Blue;
+                lblCunningLabel.ForeColor = Color.Blue;
+                lblWillpowerLabel.ForeColor = Color.Blue;
+                lblPresenceLabel.ForeColor = Color.Blue;
+                lblSoakLabel.ForeColor = Color.Blue;
+                lblWoundsLabel.ForeColor = Color.Blue;
+                lblStrainLabel.ForeColor = Color.Blue;
+                lblDefenseLabel.ForeColor = Color.Blue;
+                lblWoundT.ForeColor = Color.Blue;
+                lblWoundCurrent.ForeColor = Color.Blue;
+                lblStrainT.ForeColor = Color.Blue;
+                lblStrainCurrent.ForeColor = Color.Blue;
+                lblDefenseMeleeLabel.ForeColor = Color.Blue;
+                lblDefenseRangedLabel.ForeColor = Color.Blue;
+                lblTotalXPLabel.ForeColor = Color.Blue;
+                lblRemainingXPLabel.ForeColor = Color.Blue;
+                lblBrawnLabel.Visible = false;
+                lblAgilityLabel.Visible = false;
+                lblIntellectLabel.Visible = false;
+                lblCunningLabel.Visible = false;
+                lblWillpowerLabel.Visible = false;
+                lblPresenceLabel.Visible = false;
+                lblSoakLabel.Visible = false;
+                lblWoundsLabel.Visible = false;
+                lblStrainLabel.Visible = false;
+                lblDefenseLabel.Visible = false;
+                lblWoundT.Visible = true;
+                lblWoundCurrent.Visible = true;
+                lblStrainT.Visible = true;
+                lblStrainCurrent.Visible = true;
+                lblDefenseMeleeLabel.Visible = true;
+                lblDefenseRangedLabel.Visible = true;
+                lblTotalXPLabel.Visible = false;
+                lblRemainingXPLabel.Visible = false;
+                break;
+            case "Terrinoth":
+                lblBrawnLabel.ForeColor = Color.White;
+                lblAgilityLabel.ForeColor = Color.White;
+                lblIntellectLabel.ForeColor = Color.White;
+                lblCunningLabel.ForeColor = Color.White;
+                lblWillpowerLabel.ForeColor = Color.White;
+                lblPresenceLabel.ForeColor = Color.White;
+                lblSoakLabel.ForeColor = Color.White;
+                lblWoundsLabel.ForeColor = Color.White;
+                lblStrainLabel.ForeColor = Color.White;
+                lblDefenseLabel.ForeColor = Color.White;
+                lblWoundT.ForeColor = Color.White;
+                lblWoundCurrent.ForeColor = Color.White;
+                lblStrainT.ForeColor = Color.White;
+                lblStrainCurrent.ForeColor = Color.White;
+                lblDefenseMeleeLabel.ForeColor = Color.White;
+                lblDefenseRangedLabel.ForeColor = Color.White;
+                lblTotalXPLabel.ForeColor = Color.White;
+                lblRemainingXPLabel.ForeColor = Color.White;
+                lblBrawnLabel.Visible = true;
+                lblAgilityLabel.Visible = true;
+                lblIntellectLabel.Visible = true;
+                lblCunningLabel.Visible = true;
+                lblWillpowerLabel.Visible = true;
+                lblPresenceLabel.Visible = true;
+                lblSoakLabel.Visible = true;
+                lblWoundsLabel.Visible = true;
+                lblStrainLabel.Visible = true;
+                lblDefenseLabel.Visible = true;
+                lblWoundT.Visible = true;
+                lblWoundCurrent.Visible = true;
+                lblStrainT.Visible = true;
+                lblStrainCurrent.Visible = true;
+                lblDefenseMeleeLabel.Visible = true;
+                lblDefenseRangedLabel.Visible = true;
+                lblTotalXPLabel.Visible = true;
+                lblRemainingXPLabel.Visible = true;
+                break;
+            }
+        }
+
+
+            private void updateSkills()
         {
             linkSkill0.Text = myCharacter.Skills[0].SkillName + " (" + myCharacter.Skills[0].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[1].SkillName + " (" + myCharacter.Skills[1].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[2].SkillName + " (" + myCharacter.Skills[2].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[3].SkillName + " (" + myCharacter.Skills[3].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[4].SkillName + " (" + myCharacter.Skills[4].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[5].SkillName + " (" + myCharacter.Skills[5].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[6].SkillName + " (" + myCharacter.Skills[6].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[7].SkillName + " (" + myCharacter.Skills[7].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[8].SkillName + " (" + myCharacter.Skills[8].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[9].SkillName + " (" + myCharacter.Skills[9].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[10].SkillName + " (" + myCharacter.Skills[10].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[11].SkillName + " (" + myCharacter.Skills[11].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[12].SkillName + " (" + myCharacter.Skills[12].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[13].SkillName + " (" + myCharacter.Skills[13].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[14].SkillName + " (" + myCharacter.Skills[14].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[15].SkillName + " (" + myCharacter.Skills[15].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[16].SkillName + " (" + myCharacter.Skills[16].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[17].SkillName + " (" + myCharacter.Skills[17].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[18].SkillName + " (" + myCharacter.Skills[18].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[19].SkillName + " (" + myCharacter.Skills[19].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[20].SkillName + " (" + myCharacter.Skills[20].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[21].SkillName + " (" + myCharacter.Skills[21].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[22].SkillName + " (" + myCharacter.Skills[22].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[23].SkillName + " (" + myCharacter.Skills[23].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[24].SkillName + " (" + myCharacter.Skills[24].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[25].SkillName + " (" + myCharacter.Skills[25].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[26].SkillName + " (" + myCharacter.Skills[26].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[27].SkillName + " (" + myCharacter.Skills[27].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[28].SkillName + " (" + myCharacter.Skills[28].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[29].SkillName + " (" + myCharacter.Skills[29].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[30].SkillName + " (" + myCharacter.Skills[30].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[31].SkillName + " (" + myCharacter.Skills[31].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[32].SkillName + " (" + myCharacter.Skills[32].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[33].SkillName + " (" + myCharacter.Skills[33].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[34].SkillName + " (" + myCharacter.Skills[34].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[35].SkillName + " (" + myCharacter.Skills[35].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[36].SkillName + " (" + myCharacter.Skills[36].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[37].SkillName + " (" + myCharacter.Skills[37].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[38].SkillName + " (" + myCharacter.Skills[38].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[39].SkillName + " (" + myCharacter.Skills[39].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[40].SkillName + " (" + myCharacter.Skills[40].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[41].SkillName + " (" + myCharacter.Skills[41].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[42].SkillName + " (" + myCharacter.Skills[42].Characteristic + ")";
-            linkSkill0.Text = myCharacter.Skills[43].SkillName + " (" + myCharacter.Skills[43].Characteristic + ")";
+            linkSkill1.Text = myCharacter.Skills[1].SkillName + " (" + myCharacter.Skills[1].Characteristic + ")";
+            linkSkill2.Text = myCharacter.Skills[2].SkillName + " (" + myCharacter.Skills[2].Characteristic + ")";
+            linkSkill3.Text = myCharacter.Skills[3].SkillName + " (" + myCharacter.Skills[3].Characteristic + ")";
+            linkSkill4.Text = myCharacter.Skills[4].SkillName + " (" + myCharacter.Skills[4].Characteristic + ")";
+            linkSkill5.Text = myCharacter.Skills[5].SkillName + " (" + myCharacter.Skills[5].Characteristic + ")";
+            linkSkill6.Text = myCharacter.Skills[6].SkillName + " (" + myCharacter.Skills[6].Characteristic + ")";
+            linkSkill7.Text = myCharacter.Skills[7].SkillName + " (" + myCharacter.Skills[7].Characteristic + ")";
+            linkSkill8.Text = myCharacter.Skills[8].SkillName + " (" + myCharacter.Skills[8].Characteristic + ")";
+            linkSkill9.Text = myCharacter.Skills[9].SkillName + " (" + myCharacter.Skills[9].Characteristic + ")";
+            linkSkill10.Text = myCharacter.Skills[10].SkillName + " (" + myCharacter.Skills[10].Characteristic + ")";
+            linkSkill11.Text = myCharacter.Skills[11].SkillName + " (" + myCharacter.Skills[11].Characteristic + ")";
+            linkSkill12.Text = myCharacter.Skills[12].SkillName + " (" + myCharacter.Skills[12].Characteristic + ")";
+            linkSkill13.Text = myCharacter.Skills[13].SkillName + " (" + myCharacter.Skills[13].Characteristic + ")";
+            linkSkill14.Text = myCharacter.Skills[14].SkillName + " (" + myCharacter.Skills[14].Characteristic + ")";
+            linkSkill15.Text = myCharacter.Skills[15].SkillName + " (" + myCharacter.Skills[15].Characteristic + ")";
+            linkSkill16.Text = myCharacter.Skills[16].SkillName + " (" + myCharacter.Skills[16].Characteristic + ")";
+            linkSkill17.Text = myCharacter.Skills[17].SkillName + " (" + myCharacter.Skills[17].Characteristic + ")";
+            linkSkill18.Text = myCharacter.Skills[18].SkillName + " (" + myCharacter.Skills[18].Characteristic + ")";
+            linkSkill19.Text = myCharacter.Skills[19].SkillName + " (" + myCharacter.Skills[19].Characteristic + ")";
+            linkSkill20.Text = myCharacter.Skills[20].SkillName + " (" + myCharacter.Skills[20].Characteristic + ")";
+            linkSkill21.Text = myCharacter.Skills[21].SkillName + " (" + myCharacter.Skills[21].Characteristic + ")";
+            linkSkill22.Text = myCharacter.Skills[22].SkillName + " (" + myCharacter.Skills[22].Characteristic + ")";
+            linkSkill23.Text = myCharacter.Skills[23].SkillName + " (" + myCharacter.Skills[23].Characteristic + ")";
+            linkSkill24.Text = myCharacter.Skills[24].SkillName + " (" + myCharacter.Skills[24].Characteristic + ")";
+            linkSkill25.Text = myCharacter.Skills[25].SkillName + " (" + myCharacter.Skills[25].Characteristic + ")";
+            linkSkill26.Text = myCharacter.Skills[26].SkillName + " (" + myCharacter.Skills[26].Characteristic + ")";
+            linkSkill27.Text = myCharacter.Skills[27].SkillName + " (" + myCharacter.Skills[27].Characteristic + ")";
+            linkSkill28.Text = myCharacter.Skills[28].SkillName + " (" + myCharacter.Skills[28].Characteristic + ")";
+            linkSkill29.Text = myCharacter.Skills[29].SkillName + " (" + myCharacter.Skills[29].Characteristic + ")";
+            linkSkill30.Text = myCharacter.Skills[30].SkillName + " (" + myCharacter.Skills[30].Characteristic + ")";
+            linkSkill31.Text = myCharacter.Skills[31].SkillName + " (" + myCharacter.Skills[31].Characteristic + ")";
+            linkSkill32.Text = myCharacter.Skills[32].SkillName + " (" + myCharacter.Skills[32].Characteristic + ")";
+            linkSkill33.Text = myCharacter.Skills[33].SkillName + " (" + myCharacter.Skills[33].Characteristic + ")";
+            linkSkill34.Text = myCharacter.Skills[34].SkillName + " (" + myCharacter.Skills[34].Characteristic + ")";
+            linkSkill35.Text = myCharacter.Skills[35].SkillName + " (" + myCharacter.Skills[35].Characteristic + ")";
+            linkSkill36.Text = myCharacter.Skills[36].SkillName + " (" + myCharacter.Skills[36].Characteristic + ")";
+            linkSkill37.Text = myCharacter.Skills[37].SkillName + " (" + myCharacter.Skills[37].Characteristic + ")";
+            linkSkill38.Text = myCharacter.Skills[38].SkillName + " (" + myCharacter.Skills[38].Characteristic + ")";
+            linkSkill39.Text = myCharacter.Skills[39].SkillName + " (" + myCharacter.Skills[39].Characteristic + ")";
+            linkSkill40.Text = myCharacter.Skills[40].SkillName + " (" + myCharacter.Skills[40].Characteristic + ")";
+            linkSkill41.Text = myCharacter.Skills[41].SkillName + " (" + myCharacter.Skills[41].Characteristic + ")";
+            linkSkill42.Text = myCharacter.Skills[42].SkillName + " (" + myCharacter.Skills[42].Characteristic + ")";
+            linkSkill43.Text = myCharacter.Skills[43].SkillName + " (" + myCharacter.Skills[43].Characteristic + ")";
         }
 
         private void linkSkill_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             panelSkillDetail.Visible = true;
+
+            LinkLabel myLink;
+            myLink = (LinkLabel)sender;
+            int linkNumber = int.Parse(myLink.Name.Substring(9));
+            txtSkill.Text = myCharacter.Skills[linkNumber].SkillName;
+            cboCharacteristic.Text = myCharacter.Skills[linkNumber].Characteristic;
+            txtSkillDescription.Text = myCharacter.Skills[linkNumber].Description;
+
+            myCharacter.Skills[linkNumber].SkillName = txtSkill.Text;
+            myCharacter.Skills[linkNumber].Characteristic = cboCharacteristic.Text;
+            myCharacter.Skills[linkNumber].Description = txtSkillDescription.Text;
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
@@ -350,6 +445,18 @@ namespace Genesys_Character_Builder
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnSkills_Click(object sender, EventArgs e)
+        {
+            panelSkills.Visible = true;
+            panelTalents.Visible = false;
+        }
+
+        private void btnTalents_Click(object sender, EventArgs e)
+        {
+            panelSkills.Visible = false;
+            panelTalents.Visible = true;
         }
     }
 }
